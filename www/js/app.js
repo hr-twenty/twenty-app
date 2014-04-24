145a0f8e117d92d848b897ba460f6b83a44b7078
Hack Reactor Students <accounts@hackreactor.com>
2014-04-23 19:58:05 -0700

    Add card structure and light styling. Add functionality to menu toggle buttons.

--- www/js/app.js
+++ www/js/app.js
@@ -2,9 +2,20 @@ angular.module('twenty', ['ionic'])
 
 	.controller('TwentyCtrl', ['$scope', function($scope){
 		$scope.settings = [
-		{link:'#/availability', title: 'Availability'},
-		{link:'#/myprofile', title: 'My profile'},
-		{link:'#/gopremium', title: 'Go premium'},
+		  {link:'#/availability', title: 'Availability'},
+	  	{link:'#/myprofile', title: 'My profile'},
+  		{link:'#/gopremium', title: 'Go premium'},
 		];
+
+    $scope.userDetails = {
+      firstName: 'Phillip',
+      lastName: 'Alexander',
+      headline: 'Lead Curriculum Engineer and Instructor at Hack Reactor',
+      image: 'http://placehold.it/110x110&text=Profile+Pic',
+      sharedConns: 1,
+      lastActive: '5h',
+      company: 'Hack Reactor',
+      interestedIn: ['AngularJS', 'Angel Investing', 'JavaScript', 'Business Development', 'Mobile Apps']
+    };
 	}])
 ;
