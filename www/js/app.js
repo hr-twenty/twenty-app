06d67f4bbf6cad30e254240bbaecea7d6190b556
Ian <ilyons@gmail.com>
2014-04-24 13:52:09 -0700

    Add stub data for and create/style style settings and conversations tabs

--- www/js/app.js
+++ www/js/app.js
@@ -1,10 +1,9 @@
 angular.module('twenty', ['ionic'])
 
-	.controller('TwentyCtrl', ['$scope', function($scope){
+	.controller('TwentyCtrl', ['$scope', '$filter', function($scope, $filter){
 		$scope.settings = [
-		  {link:'#/availability', title: 'Availability'},
 	  	{link:'#/myprofile', title: 'My profile'},
-  		{link:'#/gopremium', title: 'Go premium'},
+  		{link:'#/logout', title: 'Log out'}
 		];
 
     $scope.userDetails = {
@@ -21,5 +20,41 @@ angular.module('twenty', ['ionic'])
       company: 'Hack Reactor',
       interestedIn: ['AngularJS', 'Angel Investing', 'JavaScript', 'Business Development', 'Mobile Apps']
     };
+
+    $scope.userMessages = [
+      { id: 1,
+        contactDisplayName: 'Robert H.',
+        contactCompany: 'Flashbang Media',
+        contactConnDate: new Date(),
+        contactPicture: 'https://media.licdn.com/mpr/mpr/shrink_200_200/p/6/000/1ea/073/01afa70.jpg',
+       contactMessages: [
+          { id: 'uasiokdjha',
+          text: 'How come you never talk to me?',
+          sendTime: new Date("April 23, 2014")}
+        ],
+        contactMessagePreview: function() { 
+          // this can be refactored to a ternary, also needs another helper. works for now.
+          if(this.contactMessages) {
+            return this.contactMessages[this.contactMessages.length-1].text.substring(0,30) + '...';
+          } else {
+            return 'Connected on ' + $filter('date')(this.contactConnDate, 'mmm dd, YYYY');
+          }
+        }
+      },
+      { id: 2,
+        contactDisplayName: 'Ian L.',
+        contactCompany: 'BrightTALK',
+        contactConnDate: new Date("April 24, 2013"),
+        contactPicture: 'https://media.licdn.com/mpr/mpr/shrink_200_200/p/4/000/12c/25e/32d438e.jpg',
+        contactMessagePreview: function() { 
+          // this can be refactored to a ternary, also needs another helper. works for now.
+          if(this.contactMessages) {
+            return this.contactMessages[this.contactMessages.length-1].text.substring(0,30) + '...';
+          } else {
+            return 'Connected on ' + $filter('date')(this.contactConnDate, 'MMM dd, yyyy');
+          }
+        }
+      }
+    ];
 	}])
 ;
