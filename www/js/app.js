6af6fe71eeed19fa70b1662d2775e7627ab85cf5
Ian <ilyons@gmail.com>
2014-04-23 18:37:11 -0700

    Added manu icons and a basic controller

--- www/js/app.js
+++ www/js/app.js
@@ -1,1 +1,10 @@
-angular.module('twenty', ['ionic']);
+angular.module('twenty', ['ionic'])
+
+	.controller('TwentyCtrl', ['$scope', function($scope){
+		$scope.settings = [
+		{link:'#/availability', title: 'Availability'},
+		{link:'#/myprofile', title: 'My profile'},
+		{link:'#/gopremium', title: 'Go premium'},
+		];
+	}])
+;
