44319d1ab63aeb8ca0900f6c6fb02a1f7464d2aa
Robert Holmes <rwholmes1@gmail.com>
2014-04-25 10:49:53 -0700

    Hooked up basic messages service

--- www/messages/list/list.js
+++ www/messages/list/list.js
@@ -1,39 +1,5 @@
 angular.module('app.messages.list', [])
 
-	.controller('MessagesListCtrl', ['$scope', '$filter', function($scope, $filter){
-		$scope.userMessages = [
-		  { id: 1,
-		    contactDisplayName: 'Robert H.',
-		    contactCompany: 'Flashbang Media',
-		    contactConnDate: new Date(),
-		    contactPicture: 'https://media.licdn.com/mpr/mpr/shrink_200_200/p/6/000/1ea/073/01afa70.jpg',
-		   contactMessages: [
-		      { id: 'uasiokdjha',
-		      text: 'How come you never talk to me?',
-		      sendTime: new Date("April 23, 2014")}
-		    ],
-		    contactMessagePreview: function() { 
-		      // this can be refactored to a ternary, also needs another helper. works for now.
-		      if(this.contactMessages) {
-		        return this.contactMessages[this.contactMessages.length-1].text.substring(0,30) + '...';
-		      } else {
-		        return 'Connected on ' + $filter('date')(this.contactConnDate, 'mmm dd, YYYY');
-		      }
-		    }
-		  },
-		  { id: 2,
-		    contactDisplayName: 'Ian L.',
-		    contactCompany: 'BrightTALK',
-		    contactConnDate: new Date("April 24, 2013"),
-		    contactPicture: 'https://media.licdn.com/mpr/mpr/shrink_200_200/p/4/000/12c/25e/32d438e.jpg',
-		    contactMessagePreview: function() { 
-		      // this can be refactored to a ternary, also needs another helper. works for now.
-		      if(this.contactMessages) {
-		        return this.contactMessages[this.contactMessages.length-1].text.substring(0,30) + '...';
-		      } else {
-		        return 'Connected on ' + $filter('date')(this.contactConnDate, 'MMM dd, yyyy');
-		      }
-		    }
-		  }
-		];
+	.controller('MessagesListCtrl', ['$scope', '$filter', 'Messages', function($scope, $filter, Messages){
+		$scope.userMessages = Messages.all();
 	}]);
--- www/services/messages.js
+++ www/services/messages.js
@@ -1,3 +1,52 @@
 angular.module('app.services.messages', [
   
-]);
+])
+
+.factory('Messages', function() {
+
+	// Test data
+  userMessages = [
+	  { id: 0,
+	    contactDisplayName: 'Robert H.',
+	    contactCompany: 'Flashbang Media',
+	    contactConnDate: new Date(),
+	    contactPicture: 'https://media.licdn.com/mpr/mpr/shrink_200_200/p/6/000/1ea/073/01afa70.jpg',
+	   contactMessages: [
+	      { id: 'uasiokdjha',
+	      text: 'How come you never talk to me?',
+	      sendTime: new Date("April 23, 2014")}
+	    ],
+	    contactMessagePreview: function() { 
+	      // this can be refactored to a ternary, also needs another helper. works for now.
+	      if(this.contactMessages) {
+	        return this.contactMessages[this.contactMessages.length-1].text.substring(0,30) + '...';
+	      } else {
+	        return 'Connected on ' + $filter('date')(this.contactConnDate, 'mmm dd, YYYY');
+	      }
+	    }
+	  },
+	  { id: 1,
+	    contactDisplayName: 'Ian L.',
+	    contactCompany: 'BrightTALK',
+	    contactConnDate: new Date("April 24, 2013"),
+	    contactPicture: 'https://media.licdn.com/mpr/mpr/shrink_200_200/p/4/000/12c/25e/32d438e.jpg',
+	    contactMessagePreview: function() { 
+	      // this can be refactored to a ternary, also needs another helper. works for now.
+	      if(this.contactMessages) {
+	        return this.contactMessages[this.contactMessages.length-1].text.substring(0,30) + '...';
+	      } else {
+	        return 'Connected on ' + $filter('date')(this.contactConnDate, 'MMM dd, yyyy');
+	      }
+	    }
+	  }
+	];
+
+  return {
+    all: function() {
+      return userMessages;
+    },
+    get: function(convId) {
+    	return userMessages[convId];
+    }
+  }
+});
