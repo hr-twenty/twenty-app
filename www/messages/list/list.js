e71b829d72b01236c5d18c3dedeb5873b823e11d
Robert Holmes <rwholmes1@gmail.com>
2014-04-25 11:59:34 -0700

    Fix merge issues with services

--- www/messages/list/list.js
+++ www/messages/list/list.js
@@ -1,5 +1,5 @@
 angular.module('app.messages.list', [])
 
 .controller('MessagesListCtrl', ['$scope', '$filter', 'Messages', function($scope, $filter, Messages){
-	$scope.userMessages = Messages.all();
+	$scope.conversations = Messages.all();
 }]);
