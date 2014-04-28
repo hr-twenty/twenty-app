f9333cef3170db96534cd646ce3a5a51f716204f
Ian <ilyons@gmail.com>
2014-04-27 22:53:14 -0700

    Update settings to reflect current possibilities. Update preferences to make it semi-functional once we have OAuth to log out with

--- www/settings/settings.js
+++ www/settings/settings.js
@@ -2,9 +2,10 @@ angular.module('app.settings', [])
 
 	.controller('SettingsCtrl', ['$scope', function($scope){
 		$scope.settings = [
-	  	{link:'#/myprofile', title: 'My profile', icon:'ion-person'},
-	  	{link:'#/settings', title: 'Settings', icon:'ion-gear-b'},
-  		{link:'#/logout', title: 'Log out', icon:'ion-log-out'},
+			// What does the profile edit page look like?
+	  	// {link:'/#/myprofile', title: 'My profile', icon:'ion-person'},
+	  	{link:'/#/preferences', title: 'Preferences', icon:'ion-gear-b'},
+  		{link:'/#/logout', title: 'Log out', icon:'ion-log-out'},
   		// Remove this login link later
 		];
 	}]);
