85aa461a32096692f1709f7ebafab9b700d92a08
Ian <ilyons@gmail.com>
2014-04-29 19:19:10 -0700

    OAUTH LOGIN

--- www/main/main.js
+++ www/main/main.js
@@ -1,13 +1,13 @@
 angular.module('app.main', [])
 
-.controller('MainIndexCtrl', ['$scope', '$stateParams', 'Users', 'StateControl', 'Backend', function($scope, $stateParams, Users, StateControl, Backend){
+.controller('MainIndexCtrl', ['$scope', '$location','$stateParams', 'Users', 'StateControl', function($scope, $location, $stateParams, Users, StateControl, Backend){
 
   $scope.$on('$viewContentLoaded', function() {
   	StateControl.toggleMenuByState($stateParams);
   });
 
-  Backend.get('/user', {userId: '10'}, function(data) {
-  	console.log(data);
-  });
+  if($location.$$search.userId) {
+		Users.currentUserId = $location.$$search.userId;
+  }
 
 }]);
