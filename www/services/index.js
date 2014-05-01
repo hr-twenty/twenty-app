bdd486103496192353f4f70ef4c48cc05bd768e3
Robert Holmes <rwholmes1@gmail.com>
2014-04-30 19:46:26 -0700

    Add LocalStorage Service

--- www/services/index.js
+++ www/services/index.js
@@ -2,5 +2,6 @@ angular.module('app.services', [
   'app.services.main',
   'app.services.backend',
   'app.services.messages',
-  'app.services.cards'
+  'app.services.cards',
+  'app.services.localStorage'
 ]);
---
+++ www/services/local-storage.js
@@ -0,0 +1,5 @@
+angular.module('app.services.localStorage', [])
+
+.service('LocalStorage', ['$filter', 'Users', 'Backend', function($filter, Users, Backend) {
+	
+}]);
