3c777c79b530271fa600bc4cb63a3d64f8e982fb
Addison <Addisonedwardlee@gmail.com>
2014-05-09 17:51:31 -0700

    added network connectivity check on login

--- www/login/login.js
+++ www/login/login.js
@@ -1,7 +1,7 @@
 angular.module('app.login', [])
 
-	.controller('LoginCtrl', ['$scope','$state', 'Users', 'Cards', 
-    function($scope, $state, Users, Cards) {
+	.controller('LoginCtrl', ['$scope','$state', '$ionicPopup', 'Users', 'Cards', 
+    function($scope, $state, $ionicPopup, Users, Cards) {
 		$scope.authUrl = 'http://191.236.102.40:55555/auth/linkedin';
 
     var initialize = function(){
@@ -10,6 +10,8 @@ angular.module('app.login', [])
     }();
 
     $scope.authorize = function() {
+      checkConnection();
+
       //if user doesn't exist on local storage, run auth flow
       if(!Users.currentUserId()){
         var ref = window.open($scope.authUrl, '_blank', 'location=no');
@@ -30,4 +32,25 @@ angular.module('app.login', [])
         $state.go('main.home');
       }
     };
-	}]);
+
+    var checkConnection = function(){
+      var networkState = navigator.connection.type;
+
+      var states = {};
+      states[Connection.UNKNOWN]  = 'Unknown connection';
+      states[Connection.ETHERNET] = 'Ethernet connection';
+      states[Connection.WIFI]     = 'WiFi connection';
+      states[Connection.CELL_2G]  = 'Cell 2G connection';
+      states[Connection.CELL_3G]  = 'Cell 3G connection';
+      states[Connection.CELL_4G]  = 'Cell 4G connection';
+      states[Connection.CELL]     = 'Cell generic connection';
+      states[Connection.NONE]     = 'No network connection';
+
+      if(states[networkState] === 'No network connection'){
+        $ionicPopup.alert({
+          title: 'You currently have limited or no internet connectivity, which may impact the performance of this app'
+        });
+      }
+    };
+
+}]);
