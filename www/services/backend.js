52144104f8e76526a62e81ad93d8db92e0de233c
Wesley Mao <wjmao88@gmail.com>
2014-06-06 10:55:28 -0400

    change host url

--- www/services/backend.js
+++ www/services/backend.js
@@ -2,7 +2,8 @@ angular.module('app.services.backend', [])
 
 .factory('Backend', ['$http', function($http) {
 	
-	var dbHost = 'http://191.236.102.40:55555';
+	var dbHost = 'twenty-prod.cloudapp.net:55555';
+	//var dbHost = 'http://191.236.102.40:55555';
 	// var dbHost = 'http://10.4.8.235:8000';
 
 	var request = function(verb, path, payload, callback) {
